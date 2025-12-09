4a17dfdccb83d44400ad34add72a7e4f238a1617
Adam Sasine <adam.sasine@gmail.com>
2025-12-09 15:02:24 -0800

    Added solution for part 2

--- solutions/src/bin/year2025-day09.rs
+++ solutions/src/bin/year2025-day09.rs
@@ -3,8 +3,8 @@
 //! https://adventofcode.com/2025/day/9
 
 use itertools::Itertools;
-use solutions::grid::Coordinate;
-use tracing::{instrument, trace};
+use solutions::grid::{Coordinate, Line, Rectangle};
+use tracing::{instrument, trace, warn};
 
 #[instrument(skip(input), level = "debug")]
 fn part1(input: &str) -> usize {
@@ -12,9 +12,11 @@ fn part1(input: &str) -> usize {
 
     coordinates
         .iter()
+        .copied()
         .tuple_combinations()
-        .inspect(|(a, b)| trace!(?a, ?b))
-        .map(|(a, b)| area_of(a, b))
+        .map(|(a, b)| Rectangle { a, b })
+        .inspect(|rectangle| trace!(?rectangle))
+        .map(|r| r.area())
         .inspect(|area| trace!(area))
         .max()
         .unwrap_or(0)
@@ -22,7 +24,76 @@ fn part1(input: &str) -> usize {
 
 #[instrument(skip(input), level = "debug")]
 fn part2(input: &str) -> usize {
-    0
+    // I checked and no two line segments intersect in the input data, so we don't have to worry about holes, just concavity.
+    let coordinates = iter_coordinates(input).collect::<Option<Vec<_>>>().unwrap();
+    let all_line_segments = coordinates
+        .iter()
+        .copied()
+        .circular_tuple_windows()
+        .map(|(a, b)| Line(a, b))
+        .collect_vec();
+
+    let all_rectangles = coordinates
+        .iter()
+        .copied()
+        .tuple_combinations()
+        .map(|(a, b)| Rectangle { a, b })
+        .inspect(|rectangle| trace!(?rectangle))
+        .sorted_unstable_by_key(|r| r.area())
+        .rev()
+        .collect_vec();
+
+    /// Check if an axis-aligned rectangle is valid, i.e. does not intersect with any of the line segments.
+    fn is_valid(rectangle: &Rectangle, line_segments: &[Line]) -> bool {
+        line_segments
+            .iter()
+            .all(|line| !is_intersecting(rectangle, line))
+    }
+
+    /// Check if an axis-aligned line segment intersects with an axis-aligned rectangle.
+    fn is_intersecting(rectangle: &Rectangle, line: &Line) -> bool {
+        let xmin = rectangle.a.x.min(rectangle.b.x);
+        let ymin = rectangle.a.y.min(rectangle.b.y);
+        let xmax = rectangle.a.x.max(rectangle.b.x);
+        let ymax = rectangle.a.y.max(rectangle.b.y);
+
+        let lxmin = line.0.x.min(line.1.x);
+        let lymin = line.0.y.min(line.1.y);
+        let lxmax = line.0.x.max(line.1.x);
+        let lymax = line.0.y.max(line.1.y);
+
+        if lxmin == lxmax {
+            // line is vertical
+            let lx = lxmin;
+            if xmin < lx && lx < xmax {
+                // line is strictly between the vertical edges of the rectangle
+                if (lymin <= ymin && lymax > ymin) || (lymin < ymax && lymax >= ymax) {
+                    // line intersects the top or bottom edge of the rectangle: invalid
+                    return true;
+                }
+            }
+        } else {
+            // lymin == lymax
+            // line is horizontal
+            let ly = lymin;
+            if ymin < ly && ly < ymax {
+                // line is strictly between the horizontal edges of the rectangle
+                if (lxmin <= xmin && lxmax > xmin) || (lxmin < xmax && lxmax >= xmax) {
+                    // line intersects the left or right edge of the rectangle: invalid
+                    return true;
+                }
+            }
+        }
+
+        false
+    }
+
+    // all_rectangles is sorted descending, and we want the largest, so the first valid one will do
+    all_rectangles
+        .into_iter()
+        .find(|r| is_valid(r, &all_line_segments))
+        .map(|r| r.area())
+        .unwrap_or(0)
 }
 
 fn main() {
@@ -41,11 +112,6 @@ fn iter_coordinates(input: &str) -> impl Iterator<Item = Option<Coordinate>> {
                 y: y?.parse().ok()?,
             })
         })
-}
-
-/// Calculate the area of a rectangle with opposite corners at the given coordinates.
-fn area_of(a: &Coordinate, b: &Coordinate) -> usize {
-    (a.x.abs_diff(b.x) + 1) * (a.y.abs_diff(b.y) + 1)
 }
 
 #[cfg(test)]
